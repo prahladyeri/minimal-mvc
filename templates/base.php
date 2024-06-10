f81151e8d815f90d8f543c9b8f987c12845aac18
Prahlad Yeri <prahladyeri@yahoo.com>
2024-06-10 23:39:32 +0530

    chore: added templates and static directory for stereotyping

---
+++ templates/base.php
@@ -0,0 +1,28 @@
+<?php
+/**
+* base.php
+* 
+* Base template
+* 
+* @author Prahlad Yeri <prahladyeri@yahoo.com>
+* @license MIT
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title><?=$title?></title>
+	<link rel="stylesheet" href="<?=base_url()?>static/css/app.css?v=1">
+</head>
+<body>
+<div class='main'>
+<p class='strong'>Welcome to minimal-mvc.
+<br><br>
+This is an attempt to remove the usual cruft that goes around the monstrous PHP frameworks and other software components in webdev world.
+<br><br>
+Together, we will create a more frugal and elegant world without cruft, enjoy!
+</p>
+<?php require($__content_file) ?>
+</div>
+</body>
+</html>
---
+++ templates/dummy.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* dummy.php
+* 
+* Dummy template
+* 
+* @author Prahlad Yeri <prahladyeri@yahoo.com>
+* @license MIT
+*/
+?>
+This is some dummy content.
